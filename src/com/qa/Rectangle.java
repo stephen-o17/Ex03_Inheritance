4f5905a13b3005662f0c3a6aa53025f6356b1aa0
stephen-o17 <12stephenbrowne@gmail.com>
2023-07-26 13:48:12 +0100

    initial commit

--- src/com/qa/Rectangle.java
+++ src/com/qa/Rectangle.java
@@ -3,14 +3,31 @@ package com.qa;
 public class Rectangle extends Shape{
     private double height, width;
 
-    public Rectangle(double height, double width) {
+    public Rectangle(String name, String colour, double x, double y, double height, double width) {
+        super(name, colour, x, y);
         this.height = height;
         this.width = width;
+    }
+
+    public boolean isSquare(){
+        return (width == height);
     }
 
     @Override
     public String toString(){
         return "Rectangle [width=" + width + ", height=" + height + "]";
+    }
+
+    @Override
+    public double getArea() {
+        return width*height;
+    }
+
+    @Override
+    public Point getCentrePoint() {
+        double centreX = getX() + width/2;
+        double centreY = getY() + height/2;
+        return new Point(centreX, centreY);
     }
 }
 
